5bb7b7d9415d976b6e86bf9017ab2b0ea76fd994
Avantha <avanthac@yahoo.com>
2020-08-16 19:43:50 +0530

    adding all

---
+++ src/main/java/com/example/backpacker_calculations/AddTripDetails.java
@@ -0,0 +1,17 @@
+package com.example.backpacker_calculations;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Switch;
+
+public class AddTripDetails extends AppCompatActivity {
+
+    Switch aSwitch;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trip_details);
+    }
+}
---
+++ src/main/java/com/example/backpacker_calculations/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.backpacker_calculations;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_trip_details);
+    }
+}
